6a7cbee66e6d2baa30d02495ea33a0ec97930d44
Rajatt09 <rajatbhati9559@gmail.com>
2024-04-20 00:10:41 +0530

    basic setup including connection of mongodb

---
+++ Backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+export { app };
