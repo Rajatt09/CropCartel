1a4752f6882291f45631f824c040bdb92fa02b47
Rajatt09 <rajatbhati9559@gmail.com>
2024-04-19 23:56:07 +0530

    created frontend setup

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+
+if (import.meta.env.DEV) {
+  console.log("Running in development mode");
+  axios.defaults.baseURL = import.meta.env.VITE_LOCALHOST;
+} else {
+  console.log("Running in production mode");
+  axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
