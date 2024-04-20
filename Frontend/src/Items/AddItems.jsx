78e6b158f67338d69540dae7f55476a1d7d46f82
dhruvgarg03 <gdhruv994@gmail.com>
2024-04-20 22:26:33 +0530

    edit add item

---
+++ Frontend/src/Items/AddItems.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CommonItem from "../Components/Commonitem";
+import { useState, useEffect } from "react";
+
+const AddItem = () => {
+  return (
+    <div>
+      <CommonItem name="Add" />
+    </div>
+  );
+};
+
+export default AddItem;
